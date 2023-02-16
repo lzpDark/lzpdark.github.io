f35e95f4d0c6fcc7f25624db550d097bdf9bf6a6
lzpdark <lmzhpx@gmail.com>
2023-02-17 01:13:25 +0800

    openvpn

--- docs/new.sh
+++ docs/new.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-# Prompt the user for the post title
-read -p "Enter the post title: " title
+# Get the title for the new post from the first argument
+title=$1
 
-# Generate the filename from the title
-filename=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+# If no title was provided, exit with an error message
+if [ -z "$title" ]; then
+  echo "Error: Please provide a title for the new post"
+  exit 1
+fi
 
-# Generate the current date in YYYY-MM-DD format
-date=$(date +%Y-%m-%d)
+# custom date for the new post
+date=$(date +'%Y-%m-%d')
 
-# Create the Markdown file with the necessary front matter
-echo "---" > "_posts/$date-$filename.md"
-echo "layout: post" >> "_posts/$date-$filename.md"
-echo "title: \"$title\"" >> "_posts/$date-$filename.md"
-echo "date: $date" >> "_posts/$date-$filename.md"
-echo "---" >> "_posts/$date-$filename.md"
+# Get the optional category for the new post from the third argument, if provided
+if [ -z "$2" ]; then
+  category=""
+else
+  category="categories: $2"
+fi
+
+# Generate the filename from the post title and date
+slug=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr '[:space:]' '-' | sed 's/-$//')
+filename=$(date -d "$date" +'%Y-%m-%d')-$slug".md"
+
+# Construct the YAML front matter for the new post
+front_matter=$(echo -e "---\nlayout: post\ntitle: \"$title\"\ndate: $date\n$category\n---")
+
+# Create the new post file with the front matter
+echo -e "$front_matter\n" > "_posts/$filename"
